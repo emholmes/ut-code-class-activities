e463f26613e41c771bfbad38f765fc2385a0d2fe
Erin Holmes <e.holmes09@yahoo.com>
2021-05-27 20:56:39 -0500

    solved 04-localStorage and 05-True-False-Game

--- 04-Web-APIs/01-Activities/04-localStorage/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/04-localStorage/Unsolved/assets/js/script.js
@@ -7,6 +7,17 @@ var userPasswordSpan = document.querySelector('#user-password');
 
 function renderLastRegistered() {
   // TODO: Retrieve the last email and password from localStorage
+  var savedUserInfo = localStorage.getItem("userDataObj");
+
+  if (!savedUserInfo) {
+    return false;
+  }
+
+  savedUserInfo = JSON.parse(savedUserInfo);
+
+  userEmailSpan.textContent = savedUserInfo.email;
+  userPasswordSpan.textContent = savedUserInfo.password;
+
   // TODO: If they are null, return early from this function
   // TODO: Otherwise set the text of the 'userEmailSpan' and 'userPasswordSpan' to the corresponding values from localStorage
 }
@@ -33,5 +44,18 @@ signUpButton.addEventListener('click', function(event) {
 
     // TODO: Save email and password to localStorage
     // TODO: Render the last registered email and password
-  }
+  }  
+
+  var userDataObj = {
+    email: email,
+    password: password
+  }; 
+
+  var userInfo = [];
+  userInfo.push(userDataObj);
+  localStorage.setItem("userDataObj", JSON.stringify(userDataObj));
+
+  // renderLastRegistered();
 });
+
+
--- 04-Web-APIs/01-Activities/05-True-or-False-Game/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/05-True-or-False-Game/Unsolved/assets/js/script.js
@@ -1,11 +1,41 @@
 // TODO: Create an array with five question objects
+var objectsArray = [
+    {
+        q: "Does 1 + 1 = 3?", 
+        a: false
+    },
+    {
+        q: "Does the cow go moo?",
+        a: true
+    }, 
+    {
+        q: "Does 1 + 1 = 2?",
+        a: true
+    }, 
+    {
+        q: "Is grass green?", 
+        a: true
+    },
+    {
+        q: "Is Austin the capital of Texas?",
+        a: true
+    }
+];
 
 // TODO: Create a variable to keep track of the score
-
+var score = 0;
 // TODO: Iterate over the questions array and display each question in a confirmation box
-
-// TODO: Check the user's answer against the correct answer
-
-// TODO: Alert the user if they are correct or wrong. Increment the score accordingly
+for (var i = 0; i < objectsArray.length; i++) {
+    var answer = confirm(objectsArray[i].q);
+    // TODO: Check the user's answer against the correct answer
+    if (answer === objectsArray[i].a) {
+        alert("Correct!");
+        score++;
+    } else {
+        // TODO: Alert the user if they are correct or wrong. Increment the score accordingly
+        alert("Wrong!");
+    }
+}
 
 // TODO: At the end of the game, alert the user with the final score
+alert("Your score: " + score + " / " + objectsArray.length);
