ee08927fc4163377f60ad6b25e5088030d3b89ae
Erin Holmes <e.holmes09@yahoo.com>
2021-06-01 17:12:47 -0500

    solved sum-array algorithm

--- 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,9 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function(arr) {
+    var sum = 0;
+    for (var num of arr) {
+        sum += num;
+    }
+    return sum;
+};
